371f5dd0583cbea1c6bbbf8faf020f5e370ecbc4
Edxael <zerphyr.r@gmail.com>
2018-01-07 17:53:49 -0700

    add notes files

--- game_code.js
+++ game_code.js
@@ -2,9 +2,3 @@ console.log("Game Code V: 1.0.0");
 
 const canvas = document.getElementById("myCanvas")
 const ctx = canvas.getContext('2d')
-
-ctx.beginPath()
-ctx.rect(20, 40, 50, 50)
-ctx.fillStyle = "red"
-ctx.fill()
-ctx.closePath()
