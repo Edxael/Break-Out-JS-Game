0cd6784225e5664533089c9e0bb88f0994ba930b
Edxael <zerphyr.r@gmail.com>
2018-01-07 19:07:25 -0700

    MAKE THE BALL MOVE VERTICALLY PERPETUALLY...

--- code_notes.js
+++ code_notes.js
@@ -11,12 +11,14 @@ ctx.fillStyle = "red"
 ctx.fill()
 ctx.closePath()
 
+
 ctx.beginPath()
-ctx.arc(240, 160, 10, 0, Math.PI*2, false)
 // ctx.arc(240, 160, 20, 0, 15, true)
+ctx.arc(240, 160, 10, 0, Math.PI*2, false)
 ctx.fillStyle = "green"
 ctx.fill()
 ctx.closePath()
+
 
 ctx.beginPath()
 ctx.rect(160, 10, 100, 40)
@@ -24,4 +26,43 @@ ctx.strokeStyle = "yellow"
 ctx.stroke()
 ctx.closePath()
 
+// ------------------------------------------------------------------------------
+
+
 // ==============================================================
+//     MAKE THE BALL MOVE VERTICALLY PERPETUALLY...
+
+const canvas = document.getElementById("myCanvas")
+const ctx = canvas.getContext('2d')
+
+let x = canvas.width/2
+let y = canvas.height - 30
+
+console.log(y);
+
+setInterval(() => {
+  ctx.beginPath()
+  ctx.arc(x, y, 10, 0, Math.PI*2, false)
+  ctx.fillStyle = "Black"
+  ctx.fill()
+  ctx.closePath()
+
+  if(y < 20){
+    y = canvas.height - 30
+  }else{
+    y -= 15
+  }
+
+  console.log(y);
+
+
+  ctx.beginPath()
+  ctx.arc(x, y, 10, 0, Math.PI*2, false)
+  ctx.fillStyle = "purple"
+  ctx.fill()
+  ctx.closePath()
+
+
+}, 500)
+
+// ------------------------------------------------------------------------------
--- game_code.js
+++ game_code.js
@@ -2,3 +2,33 @@ console.log("Game Code V: 1.0.0");
 
 const canvas = document.getElementById("myCanvas")
 const ctx = canvas.getContext('2d')
+
+let x = canvas.width/2
+let y = canvas.height - 30
+
+console.log(y);
+
+setInterval(() => {
+  ctx.beginPath()
+  ctx.arc(x, y, 10, 0, Math.PI*2, false)
+  ctx.fillStyle = "Black"
+  ctx.fill()
+  ctx.closePath()
+
+  if(y < 20){
+    y = canvas.height - 30
+  }else{
+    y -= 15
+  }
+
+  console.log(y);
+
+
+  ctx.beginPath()
+  ctx.arc(x, y, 10, 0, Math.PI*2, false)
+  ctx.fillStyle = "purple"
+  ctx.fill()
+  ctx.closePath()
+
+
+}, 500)
