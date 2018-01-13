70463ec7981fc5c7b8a1d51898202f28096b3784
Edxael <zerphyr.r@gmail.com>
2018-01-12 18:11:24 -0700

    make ball bounce paddle

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ let y = canvas.height - 30
 let dx = 2
 let dy = -2
 let ballRadius = 10
-let paddleHeight = 10
+let paddleHeight = 3
 let paddleWidth = 75
 let paddleX = (canvas.width - paddleWidth) / 2
 
@@ -36,30 +36,51 @@ const drawBall = () => {
 
 const drawPaddle = () => {
   ctx.beginPath()
-  ctx.rect(paddleX, canvas.height - paddleHeight - 8, paddleWidth, paddleHeight)
+  ctx.rect(paddleX, canvas.height - paddleHeight - 4, paddleWidth, paddleHeight)
   ctx.fillStyle = "aquamarine"
   ctx.fill()
   ctx.closePath()
 }
 
-const keyDownHandler = (e) => {
+function keyDownHandler (e) {
   if(e.keyCode == 39){ rightPressed = true }
   if(e.keyCode == 37){ leftPressed = true }
 }
-const keyUpHandler = (e) => {
+function keyUpHandler (e) {
   if(e.keyCode == 39){ rightPressed = false }
   if(e.keyCode == 37){ leftPressed = false }
 }
 
 
 const draw = () => {
+  // console.log(y);
   ctx.clearRect(0, 0, canvas.width, canvas.height)
   drawBall()
   drawPaddle()
 
   // Border Coallition Detection direction change.
-  if((y + dy < ballRadius) || (y + dy > canvas.height - ballRadius)){ dy = -dy }
+  // if((y + dy < ballRadius) || (y + dy > canvas.height - ballRadius)){ dy = -dy }
+  if( y + dy < ballRadius ){ dy = -dy }
+  else if (y + dy > canvas.height - ballRadius) {
+
+    if(x > paddleX && x < paddleX + paddleWidth ){
+      dy = -dy
+    }else{
+      alert("GAME OVER...")
+      x = canvas.width/2
+      y = canvas.height - 30
+      dx = 2
+      dy = -2
+    }
+
+  }
+
   if((x + dx < ballRadius) || (x + dx > canvas.width - ballRadius)){ dx = -dx }
+
+  // Moving the Paddle when keys are pressed
+  if(rightPressed && (paddleX <= canvas.width - paddleWidth)){ paddleX += 5 }
+  if(leftPressed && (paddleX >= 0)){ paddleX -= 5 }
+
 
   // Changing the location of the ball
   x += dx
@@ -71,4 +92,4 @@ const draw = () => {
 
 // -------------------------------------------------------------------------------------------------
 // CODE EXECUTION
-setInterval(draw, 20)
+setInterval(draw, 10)
